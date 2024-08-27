7352baa41cf11caec285ac5b68588b7535167e44
Mario Masciovecchio <mario.masciovecchio@cern.ch>
2024-08-27 07:43:59 -0700

    MkFitOutputConverter fix for phase-2, plus other adjustments in Config and MkFinder

--- RecoTracker/MkFitCore/interface/Config.h
+++ RecoTracker/MkFitCore/interface/Config.h
@@ -50,7 +50,9 @@ namespace mkfit {
     constexpr int Niter = 5;
     constexpr bool useTrigApprox = true;
     constexpr bool usePropToPlane = true;
-    constexpr bool usePtMultScat = false;
+    constexpr bool usePtMultScat = true;
+    //constexpr bool usePropToPlane = false;
+    //constexpr bool usePtMultScat = false;
 
     // Config for Bfield. Note: for now the same for CMS-phase1 and CylCowWLids.
     constexpr float Bfield = 3.8112;
